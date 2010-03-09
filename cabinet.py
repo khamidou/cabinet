e183b02c590f2ceee206d3ee25af4aa75491b562
khamidou <khamidou@gmail.com>
2010-03-09 07:38:43 +0100

    fixed an embarassing assignment bug

--- cabinet.py
+++ cabinet.py
@@ -10,18 +10,25 @@ except ImportError:
 class Cabinet(object):
     def __init__(self, filename, *args):
         self.db = bsddb.hashopen(filename)
-    
+        self.cache = {}
+
     def __getitem__(self, key):
-        if self.db.has_key(key):
-            return json.loads(self.db[key])
+        if self.cache.has_key(key):
+            return self.cache[key]
+        elif self.db.has_key(key):
+            self.cache[key] = json.loads(self.db[key])
+            return self.cache[key]
         else:
             raise KeyError
 
     def __setitem__(self, key, item):
-        self.db[key] = json.dumps(item)
+        self.cache[key] = item
 
     def __delitem__(self, key):
         del self.db[key]
 
     def sync(self):
+        for key in self.cache:
+            self.db[key] = json.dumps(self.cache[key])
+
         self.db.sync()
